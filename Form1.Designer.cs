0df4f8523fc2cdd9a0b673b5d8d1868c34d6a2cf
Kashyap Bangali <kashyapbengali@gmail.com>
2025-08-24 12:31:08 +0530

    First version

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace LoudnessWarning
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCurrentLevel = new System.Windows.Forms.Label();
+            this.lblWarningLimit = new System.Windows.Forms.Label();
+            this.txtWarningLimit = new System.Windows.Forms.TextBox();
+            this.btnSetLimit = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.pnlSoundBar = new System.Windows.Forms.Panel();
+            this.lblDbUnit = new System.Windows.Forms.Label();
+            this.lblLimitUnit = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCurrentLevel
+            // 
+            this.lblCurrentLevel.AutoSize = true;
+            this.lblCurrentLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCurrentLevel.Location = new System.Drawing.Point(12, 32);
+            this.lblCurrentLevel.Name = "lblCurrentLevel";
+            this.lblCurrentLevel.Size = new System.Drawing.Size(178, 20);
+            this.lblCurrentLevel.TabIndex = 0;
+            this.lblCurrentLevel.Text = "Current Level: 0.0 dB";
+            // 
+            // lblWarningLimit
+            // 
+            this.lblWarningLimit.AutoSize = true;
+            this.lblWarningLimit.Location = new System.Drawing.Point(12, 9);
+            this.lblWarningLimit.Name = "lblWarningLimit";
+            this.lblWarningLimit.Size = new System.Drawing.Size(110, 15);
+            this.lblWarningLimit.TabIndex = 1;
+            this.lblWarningLimit.Text = "Warning Limit (dB):";
+            // 
+            // txtWarningLimit
+            // 
+            this.txtWarningLimit.Location = new System.Drawing.Point(128, 6);
+            this.txtWarningLimit.Name = "txtWarningLimit";
+            this.txtWarningLimit.Size = new System.Drawing.Size(80, 23);
+            this.txtWarningLimit.TabIndex = 2;
+            this.txtWarningLimit.Text = "80";
+            // 
+            // btnSetLimit
+            // 
+            this.btnSetLimit.Location = new System.Drawing.Point(241, 6);
+            this.btnSetLimit.Name = "btnSetLimit";
+            this.btnSetLimit.Size = new System.Drawing.Size(75, 23);
+            this.btnSetLimit.TabIndex = 3;
+            this.btnSetLimit.Text = "Set Limit";
+            this.btnSetLimit.UseVisualStyleBackColor = true;
+            this.btnSetLimit.Click += new System.EventHandler(this.btnSetLimit_Click);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStartStop.Location = new System.Drawing.Point(12, 133);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(626, 48);
+            this.btnStartStop.TabIndex = 4;
+            this.btnStartStop.Text = "Start Monitoring";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // pnlSoundBar
+            // 
+            this.pnlSoundBar.BackColor = System.Drawing.Color.LightGray;
+            this.pnlSoundBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSoundBar.Location = new System.Drawing.Point(12, 55);
+            this.pnlSoundBar.Name = "pnlSoundBar";
+            this.pnlSoundBar.Size = new System.Drawing.Size(544, 55);
+            this.pnlSoundBar.TabIndex = 5;
+            this.pnlSoundBar.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSoundBar_Paint);
+            // 
+            // lblDbUnit
+            // 
+            this.lblDbUnit.AutoSize = true;
+            this.lblDbUnit.Location = new System.Drawing.Point(214, 9);
+            this.lblDbUnit.Name = "lblDbUnit";
+            this.lblDbUnit.Size = new System.Drawing.Size(21, 15);
+            this.lblDbUnit.TabIndex = 6;
+            this.lblDbUnit.Text = "dB";
+            // 
+            // lblLimitUnit
+            // 
+            this.lblLimitUnit.AutoSize = true;
+            this.lblLimitUnit.Location = new System.Drawing.Point(562, 75);
+            this.lblLimitUnit.Name = "lblLimitUnit";
+            this.lblLimitUnit.Size = new System.Drawing.Size(76, 15);
+            this.lblLimitUnit.TabIndex = 7;
+            this.lblLimitUnit.Text = "0 dB - 100 dB";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStatus.ForeColor = System.Drawing.Color.Green;
+            this.lblStatus.Location = new System.Drawing.Point(12, 113);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(140, 17);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Status: Monitoring";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 193);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblLimitUnit);
+            this.Controls.Add(this.lblDbUnit);
+            this.Controls.Add(this.pnlSoundBar);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.btnSetLimit);
+            this.Controls.Add(this.txtWarningLimit);
+            this.Controls.Add(this.lblWarningLimit);
+            this.Controls.Add(this.lblCurrentLevel);
+            this.Name = "Form1";
+            this.Text = "Loudness Warning Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrentLevel;
+        private System.Windows.Forms.Label lblWarningLimit;
+        private System.Windows.Forms.TextBox txtWarningLimit;
+        private System.Windows.Forms.Button btnSetLimit;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Panel pnlSoundBar;
+        private System.Windows.Forms.Label lblDbUnit;
+        private System.Windows.Forms.Label lblLimitUnit;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.IO;
+using System.Reflection;
+using System.Media;
+
+namespace LoudnessWarning
+{
+    public partial class Form1 : Form
+    {
+        private WaveInEvent waveIn;
+        private double currentDbLevel = 0;
+        private int barLevel = 0;
+        private int barLevelTarget = 0;
+        private double warningLimitDb = 80;
+        private bool isMonitoring = false;
+        private bool isAlarmPlaying = false;
+        private SoundPlayer alarmPlayer;
+        private DateTime lastAlarmTime = DateTime.MinValue;
+        private const int ALARM_COOLDOWN_MS = 10000; // 10 seconds cooldown between alarms
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAudio();
+            lblStatus.Text = "Status: Ready";
+            lblStatus.ForeColor = Color.Blue;
+        }
+
+        private void InitializeAudio()
+        {
+            try
+            {
+                // Initialize wave input for microphone
+                waveIn = new WaveInEvent();
+                waveIn.WaveFormat = new WaveFormat(44100, 1); // 44.1kHz, mono
+                waveIn.BufferMilliseconds = 100;
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+
+                // Initialize alarm sound player
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "LoudnessWarning.resources.Alarm.wav";
+                var stream = assembly.GetManifestResourceStream(resourceName);
+                
+                if (stream != null)
+                {
+                    alarmPlayer = new SoundPlayer(stream);
+                    alarmPlayer.LoadCompleted += (s, e) => { };
+                    alarmPlayer.LoadAsync();
+                }
+                else
+                {
+                    // Fallback: try to load from file system
+                    string alarmPath = Path.Combine(Application.StartupPath, "resources", "Alarm.wav");
+                    if (File.Exists(alarmPath))
+                    {
+                        alarmPlayer = new SoundPlayer(alarmPath);
+                        alarmPlayer.LoadAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing audio: {ex.Message}", "Audio Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (isAlarmPlaying) return; // Skip processing during alarm to avoid feedback loop
+
+            // Calculate RMS (Root Mean Square) for volume level
+            float sum = 0;
+            for (int i = 0; i < e.BytesRecorded; i += 2)
+            {
+                short sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i]);
+                float normalizedSample = sample / 32768f;
+                sum += normalizedSample * normalizedSample;
+            }
+
+            float rms = (float)Math.Sqrt(sum / (e.BytesRecorded / 2));
+            
+            // Convert to decibels (with some baseline to avoid log(0))
+            currentDbLevel = 20 * Math.Log10(Math.Max(rms, 0.00001)) + 94; // Add offset to get reasonable dB values
+            
+            // Clamp values to reasonable range
+            currentDbLevel = Math.Max(0, Math.Min(120, currentDbLevel));
+
+            // Update UI on main thread
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateUI));
+            }
+            else
+            {
+                UpdateUI();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            lblCurrentLevel.Text = $"Current Level: {currentDbLevel:F1} dB";
+            
+            // Check for warning condition
+            if (currentDbLevel > warningLimitDb && !isAlarmPlaying)
+            {
+                TriggerAlarm();
+            }
+            
+            // Update sound bar
+            pnlSoundBar.Invalidate();
+        }
+
+        private void TriggerAlarm()
+        {
+            // Check cooldown to prevent rapid alarm triggering
+            if (DateTime.Now.Subtract(lastAlarmTime).TotalMilliseconds < ALARM_COOLDOWN_MS)
+                return;
+
+            if (alarmPlayer != null)
+            {
+                isAlarmPlaying = true;
+                lblStatus.Text = "Status: WARNING - LOUD SOUND DETECTED!";
+                lblStatus.ForeColor = Color.Red;
+                
+                try
+                {
+                    alarmPlayer.Play();
+                    lastAlarmTime = DateTime.Now;
+                    
+                    // Reset alarm status after 1 second
+                    Timer resetTimer = new Timer();
+                    resetTimer.Interval = 1000;
+                    resetTimer.Tick += (s, e) => {
+                        isAlarmPlaying = false;
+                        if (isMonitoring)
+                        {
+                            lblStatus.Text = "Status: Monitoring";
+                            lblStatus.ForeColor = Color.Green;
+                        }
+                        resetTimer.Stop();
+                        resetTimer.Dispose();
+                    };
+                    resetTimer.Start();
+                }
+                catch (Exception ex)
+                {
+                    isAlarmPlaying = false;
+                    MessageBox.Show($"Error playing alarm: {ex.Message}", "Alarm Error", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!isMonitoring)
+                {
+                    waveIn.StartRecording();
+                    timer1.Start();
+                    isMonitoring = true;
+                    btnStartStop.Text = "Stop Monitoring";
+                    lblStatus.Text = "Status: Monitoring";
+                    lblStatus.ForeColor = Color.Green;
+                }
+                else
+                {
+                    waveIn.StopRecording();
+                    timer1.Stop();
+                    isMonitoring = false;
+                    btnStartStop.Text = "Start Monitoring";
+                    lblStatus.Text = "Status: Stopped";
+                    lblStatus.ForeColor = Color.Blue;
+                    currentDbLevel = 0;
+                    pnlSoundBar.Invalidate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error controlling microphone: {ex.Message}", "Microphone Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSetLimit_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtWarningLimit.Text, out double newLimit))
+            {
+                if (newLimit >= 0 && newLimit <= 120)
+                {
+                    warningLimitDb = newLimit;
+                    MessageBox.Show($"Warning limit set to {warningLimitDb} dB", "Limit Updated", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a value between 0 and 120 dB", "Invalid Range", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number", "Invalid Input", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void pnlSoundBar_Paint(object sender, PaintEventArgs e)
+        {
+            Panel panel = sender as Panel;
+            Graphics g = e.Graphics;
+            
+            // Clear the panel
+            g.Clear(Color.LightGray);
+            
+            if (!isMonitoring && currentDbLevel == 0) return;
+
+            // Calculate bar width based on current level (0-120 dB range)
+            int maxWidth = panel.Width - 4;
+            int barWidth = (int)((barLevel / 1000.0) * maxWidth);
+            barWidth = Math.Max(0, Math.Min(maxWidth, barWidth));
+
+            // Determine color based on warning threshold
+            Color barColor;
+            if (currentDbLevel > warningLimitDb)
+            {
+                barColor = Color.Orange; // Amber color for warning
+            }
+            else if (currentDbLevel > warningLimitDb * 0.8)
+            {
+                barColor = Color.Yellow; // Yellow for approaching warning
+            }
+            else
+            {
+                barColor = Color.Green; // Green for normal levels
+            }
+
+            // Draw the sound level bar
+            if (barWidth > 0)
+            {
+                using (SolidBrush brush = new SolidBrush(barColor))
+                {
+                    g.FillRectangle(brush, 2, 2, barWidth, panel.Height - 4);
+                }
+            }
+
+            // Draw warning threshold line
+            int warningX = (int)((warningLimitDb / 120.0) * maxWidth) + 2;
+            if (warningX > 2 && warningX < panel.Width - 2)
+            {
+                using (Pen pen = new Pen(Color.Red, 2))
+                {
+                    g.DrawLine(pen, warningX, 0, warningX, panel.Height);
+                }
+            }
+
+            // Draw scale marks
+            using (Pen pen = new Pen(Color.Black, 1))
+            {
+                for (int db = 0; db <= 120; db += 20)
+                {
+                    int x = (int)((db / 120.0) * maxWidth) + 2;
+                    g.DrawLine(pen, x, panel.Height - 10, x, panel.Height);
+                    
+                    // Draw scale labels
+                    using (Font font = new Font("Arial", 7))
+                    using (SolidBrush textBrush = new SolidBrush(Color.Black))
+                    {
+                        string label = db.ToString();
+                        SizeF textSize = g.MeasureString(label, font);
+                        g.DrawString(label, font, textBrush, x - textSize.Width / 2, panel.Height - 25);
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Timer is used to refresh the UI periodically
+            // Main audio processing happens in WaveIn_DataAvailable
+            barLevelTarget = (int)(currentDbLevel / 120 * 1000);
+            if (barLevel > barLevelTarget)
+                barLevel -= 6;
+            else
+                barLevel += 20;
+
+            // Saturate
+            if (barLevel < 0)
+                barLevel = 0;
+            else if (barLevel > 1000)
+                barLevel = 1000;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (waveIn != null)
+                {
+                    waveIn.StopRecording();
+                    waveIn.Dispose();
+                }
+                
+                if (alarmPlayer != null)
+                {
+                    alarmPlayer.Stop();
+                    alarmPlayer.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log error but don't prevent form from closing
+                System.Diagnostics.Debug.WriteLine($"Error during cleanup: {ex.Message}");
+            }
+        }
+    }
+}
